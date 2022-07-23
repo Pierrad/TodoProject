fe13683e623b0ffc57ea1856e4e5e04486fdeba2
PA <pierreadrien.vasseur@gmail.com>
2022-07-23 09:22:04 +0200

    💫 Add animations to TaskBoard & Settings

--- apps/web/src/app/components/TaskBoard/index.tsx
+++ apps/web/src/app/components/TaskBoard/index.tsx
@@ -2,6 +2,9 @@ import React, { useState } from 'react'
 import { ExpandLess, ExpandMore } from '@mui/icons-material'
 import { Collapse } from '@mui/material'
 import { useTranslation } from 'react-i18next'
+import { useSpring, animated } from 'react-spring'
+
+
 import AddTask from '../AddTask'
 import Task, { TaskProps } from '../Task'
 import TaskDetailDialog from '../TaskDetailDialog'
@@ -21,6 +24,9 @@ const TaskBoard = (props: TaskBoardProps) => {
   const [currentTask, setCurrentTask] = useState<TaskProps | null>(null)
   const [isTaskDetailDialogOpen, setIsTaskDetailDialogOpen] = useState(false)
   const [hasOpenCompletedTasks, setHasOpenCompletedTasks] = useState(false)
+  const animationProps1 = useSpring({ from: { opacity: 0, y: 20 }, to: { opacity: 1, y: 0 }, delay: 200 })
+  const animationProps2 = useSpring({ from: { opacity: 0, y: 20 }, to: { opacity: 1, y: 0 }, delay: 400 })
+  const animationProps3 = useSpring({ from: { opacity: 0, y: 20 }, to: { opacity: 1, y: 0 }, delay: 600 })
 
   const handleTaskDetailDialog = (task: TaskProps) => {
     setCurrentTask(task)
@@ -29,33 +35,39 @@ const TaskBoard = (props: TaskBoardProps) => {
 
   return (
     <SC.Container className={className}>
-      <SC.Title>{title}</SC.Title>
-      <AddTask />
-      <SC.Tasks>
-        {tasks && tasks.length > 0 && tasks.map((task, index) => (
-          <Task key={index} {...task} onClick={() => handleTaskDetailDialog(task)} />
-        ))}
-      </SC.Tasks>
-      <SC.CompletedTasks>
-        {completedTasks && completedTasks.length > 0 && (
-          <>
-            <SC.CompletedTasksTitle onClick={() => setHasOpenCompletedTasks(!hasOpenCompletedTasks)}>
-              {t('completedTasks', { count: completedTasks.length })}
-              {hasOpenCompletedTasks ? <ExpandLess /> : <ExpandMore />}
-            </SC.CompletedTasksTitle>
-            <Collapse in={hasOpenCompletedTasks} timeout="auto">
-              <SC.Tasks>
-                {completedTasks.map((task, index) => (
-                  <Task key={index} {...task} />
-                ))}
-              </SC.Tasks>
-            </Collapse>
-          </>
-        )}
-      </SC.CompletedTasks>
+      <animated.div style={animationProps1}>
+        <SC.Title>{title}</SC.Title>
+        <AddTask />
+      </animated.div>
+      <animated.div style={animationProps2}>
+        <SC.Tasks>
+          {tasks && tasks.length > 0 && tasks.map((task, index) => (
+            <Task key={index} {...task} onClick={() => handleTaskDetailDialog(task)} />
+          ))}
+        </SC.Tasks>
+      </animated.div>
+      <animated.div style={animationProps3}>
+        <SC.CompletedTasks>
+          {completedTasks && completedTasks.length > 0 && (
+            <>
+              <SC.CompletedTasksTitle onClick={() => setHasOpenCompletedTasks(!hasOpenCompletedTasks)}>
+                {t('completedTasks', { count: completedTasks.length })}
+                {hasOpenCompletedTasks ? <ExpandLess /> : <ExpandMore />}
+              </SC.CompletedTasksTitle>
+              <Collapse in={hasOpenCompletedTasks} timeout="auto">
+                <SC.Tasks>
+                  {completedTasks.map((task, index) => (
+                    <Task key={index} {...task} />
+                  ))}
+                </SC.Tasks>
+              </Collapse>
+            </>
+          )}
+        </SC.CompletedTasks>
+      </animated.div>
       <TaskDetailDialog onClose={() => setIsTaskDetailDialogOpen(false)} open={isTaskDetailDialogOpen} task={currentTask} />
     </SC.Container>
   )
-}  
+}
 
 export default TaskBoard
--- apps/web/src/app/pages/Settings/index.tsx
+++ apps/web/src/app/pages/Settings/index.tsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react'
 import { MenuItem, Select, SelectChangeEvent, ToggleButton, useTheme } from '@mui/material'
 import { useTranslation } from 'react-i18next'
 import { Pie, PieChart, ResponsiveContainer } from 'recharts'
+import { useSpring, animated } from 'react-spring'
 
 import * as SC from './styled'
 
@@ -10,7 +11,10 @@ const Settings = () => {
   const globalTheme = useTheme()
   const [language, setLanguage] = useState('fr')
   const [theme, setTheme] = useState('light')
-  const [statGroup, setStatGroup] = useState('');
+  const [statGroup, setStatGroup] = useState('')
+  const animationProps1 = useSpring({ from: { opacity: 0, y: 20 }, to: { opacity: 1, y: 0 }, delay: 200 })
+  const animationProps2 = useSpring({ from: { opacity: 0, y: 20 }, to: { opacity: 1, y: 0 }, delay: 400 })
+  const animationProps3 = useSpring({ from: { opacity: 0, y: 20 }, to: { opacity: 1, y: 0 }, delay: 600 })
 
   const handleLanguageChange = (
     event: React.MouseEvent<HTMLElement>,
@@ -48,95 +52,101 @@ const Settings = () => {
   return (
     <SC.Container>
       <SC.Settings>
-        <SC.User>
-          <SC.UserAvatarBox>
-            <SC.UserAvatar
-              src="assets/images/avatar.jpg"
-              alt="avatar"
-            />
-            <SC.ActionButton
-              variant="outlined"
+        <animated.div style={animationProps1}>
+          <SC.User>
+            <SC.UserAvatarBox>
+              <SC.UserAvatar
+                src="assets/images/avatar.jpg"
+                alt="avatar"
+              />
+              <SC.ActionButton
+                variant="outlined"
+                color="primary"
+              >
+                {t('settings_user_change_picture')}
+                <input hidden accept="image/*" multiple type="file" />
+              </SC.ActionButton>
+            </SC.UserAvatarBox>
+            <SC.UserInfo>
+              <SC.Input
+                name="name"
+                label={t('settings_user_input_name_label')}
+                variant="outlined"
+                defaultValue="John Doe"
+                size="small"
+              />
+              <SC.Input
+                name="email"
+                label={t('settings_user_input_email_label')}
+                variant="outlined"
+                defaultValue="test@gmail.com"
+                size="small"
+              />
+              <SC.ActionButton
+                variant="contained"
+                color="primary"
+              >
+                {t('settings_user_save')}
+              </SC.ActionButton>
+            </SC.UserInfo>
+          </SC.User>
+        </animated.div>
+        <animated.div style={animationProps2}>
+          <SC.Front>
+            <SC.FrontTitle variant="h6">{t('settings_front_language_label')}</SC.FrontTitle>
+            <SC.ToggleGroup
               color="primary"
+              value={language}
+              exclusive
+              onChange={handleLanguageChange}
             >
-              {t('settings_user_change_picture')}
-              <input hidden accept="image/*" multiple type="file" />
-            </SC.ActionButton>
-          </SC.UserAvatarBox>
-          <SC.UserInfo>
-            <SC.Input
-              name="name"
-              label={t('settings_user_input_name_label')}
-              variant="outlined"
-              defaultValue="John Doe"
-              size="small"
-            />
-            <SC.Input
-              name="email"
-              label={t('settings_user_input_email_label')}
-              variant="outlined"
-              defaultValue="test@gmail.com"
-              size="small"
-            />
-            <SC.ActionButton
-              variant="contained"
+              <ToggleButton value="fr">{t('settings_front_language_fr')}</ToggleButton>
+              <ToggleButton value="en">{t('settings_front_language_en')}</ToggleButton>
+            </SC.ToggleGroup>
+            <SC.FrontTitle variant="h6">{t('settings_front_theme_label')}</SC.FrontTitle>
+            <SC.ToggleGroup
               color="primary"
+              value={theme}
+              exclusive
+              onChange={handleThemeChange}
             >
-              {t('settings_user_save')}
-            </SC.ActionButton>
-          </SC.UserInfo>
-        </SC.User>
-        <SC.Front>
-          <SC.FrontTitle variant="h6">{t('settings_front_language_label')}</SC.FrontTitle>
-          <SC.ToggleGroup
-            color="primary"
-            value={language}
-            exclusive
-            onChange={handleLanguageChange}
-          >
-            <ToggleButton value="fr">{t('settings_front_language_fr')}</ToggleButton>
-            <ToggleButton value="en">{t('settings_front_language_en')}</ToggleButton>
-          </SC.ToggleGroup>
-          <SC.FrontTitle variant="h6">{t('settings_front_theme_label')}</SC.FrontTitle>
-          <SC.ToggleGroup
-            color="primary"
-            value={theme}
-            exclusive
-            onChange={handleThemeChange}
-          >
-            <ToggleButton value="light">{t('settings_front_theme_light')}</ToggleButton>
-            <ToggleButton value="dark">{t('settings_front_theme_dark')}</ToggleButton>
-          </SC.ToggleGroup>
-        </SC.Front>
+              <ToggleButton value="light">{t('settings_front_theme_light')}</ToggleButton>
+              <ToggleButton value="dark">{t('settings_front_theme_dark')}</ToggleButton>
+            </SC.ToggleGroup>
+          </SC.Front>
+        </animated.div>
       </SC.Settings>
-      <SC.Statistics>
-        <SC.StatisticsTop>
-          <SC.StatisticsTitle variant="h6">{t('settings_statistics_label')}</SC.StatisticsTitle>
-          <Select
-            id="selectGroup"
-            value={statGroup}
-            label="Groupe"
-            onChange={handleStatGroupChange}
-            size="small"
-          >
-            <MenuItem value={"IUT"}>IUT</MenuItem>
-            <MenuItem value={"Vie"}>Vie</MenuItem>
-            <MenuItem value={"Pro"}>Pro</MenuItem>
-          </Select>
-        </SC.StatisticsTop>
-        <ResponsiveContainer width="100%" height={250}>
-          <PieChart width={500} height={250}>
-            <Pie
-              data={data01}
-              dataKey="value"
-              nameKey="name"
-              cx="50%"
-              cy="50%"
-              fill={globalTheme.palette.primary.main}
-              label={(entry) => entry.name}
-            />
-          </PieChart>
-        </ResponsiveContainer>
-      </SC.Statistics>
+      <animated.div style={animationProps3}>
+        <SC.Statistics>
+          <SC.StatisticsTop>
+            <SC.StatisticsTitle variant="h6">{t('settings_statistics_label')}</SC.StatisticsTitle>
+            <Select
+              id="selectGroup"
+              value={statGroup}
+              label="Groupe"
+              onChange={handleStatGroupChange}
+              size="small"
+            >
+              <MenuItem value={"IUT"}>IUT</MenuItem>
+              <MenuItem value={"Vie"}>Vie</MenuItem>
+              <MenuItem value={"Pro"}>Pro</MenuItem>
+            </Select>
+          </SC.StatisticsTop>
+          <ResponsiveContainer width="100%" height={250}>
+            <PieChart width={500} height={250}>
+              <Pie
+                data={data01}
+                dataKey="value"
+                nameKey="name"
+                cx="50%"
+                cy="50%"
+                fill={globalTheme.palette.primary.main}
+                label={(entry) => entry.name}
+              />
+            </PieChart>
+          </ResponsiveContainer>
+        </SC.Statistics>
+      </animated.div>
     </SC.Container>
   )
 }
